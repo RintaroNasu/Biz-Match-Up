26638ba884f18a3eb9e86a4a54aa928c5a4dae0e
RintaroNasu <rinta022823@gmail.com>
2025-03-31 18:20:16 +0900

    fix:nextバージョン15に合わせてparamsの受け取り方変更

--- src/app/api/profile/[userId]/route.ts
+++ src/app/api/profile/[userId]/route.ts
@@ -3,15 +3,15 @@ import prisma from '@/lib/prisma';
 
 export async function POST(
   req: NextRequest,
-  context: { params: { userId: string } },
+  { params }: { params: { userId: string } },
 ) {
+  const userId = (await params).userId;
+  const parsedUserId = parseInt(userId);
   const body = await req.json();
-
-  const userId = parseInt(context.params.userId);
 
   try {
     const updatedUser = await prisma.user.update({
-      where: { id: userId },
+      where: { id: parsedUserId },
       data: body,
     });
 
