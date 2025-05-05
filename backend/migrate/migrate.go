26c2b546a3e62382325069df55d6447daa9d89f3
RintaroNasu <rinta022823@gmail.com>
2025-05-05 17:46:48 +0900

    feat:DB接続作成

---
+++ backend/migrate/migrate.go
@@ -0,0 +1,14 @@
+package main
+
+import (
+	"backend/db"
+	"backend/models"
+	"fmt"
+)
+
+func main() {
+	dbConn := db.NewDB()
+	defer fmt.Println("Successfully Migrated")
+	defer db.CloseDB(dbConn)
+	dbConn.AutoMigrate(&models.User{})
+}
